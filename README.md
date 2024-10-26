47ad561bf741fe8c229d18499306febb0b2a7236
Warmiko <mikowarc@gmail.com>
2024-10-27 01:11:26 +0200

    initial commit + setup

---
+++ README.md
@@ -0,0 +1,1 @@
+"This project is about a trigger detection system using pyaudio for real time trigger detection and RNN with GRU and LSTM" 
---
+++ audio_preprocessing.py
@@ -0,0 +1,10 @@
+# imports
+
+import pyaudio as pa
+import matplotlib as mp
+import random
+import numpy as np
+
+if __name__ == '__main__':
+main():
+
