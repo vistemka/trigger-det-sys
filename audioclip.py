908af46306af6e385be1cd272a11f6ff5d22dfde
vistemka <viistemka@gmail.com>
2024-10-30 22:01:00 +0100

    added audioclipping script

---
+++ audioclip.py
@@ -0,0 +1,17 @@
+from pydub import AudioSegment
+import os
+
+clipdurationMs
+fCounter
+
+
+###
+valid filepath checking here
+###:
+
+
+    for au_file in files: 
+        sound = au_file.open_as_mp3(au_file)
+        for clip in sound[::clipDurationMs]:
+            clip.export({basepath} + {u_file} + str(fCounter), format="mp3"
+            fCounter+=1
